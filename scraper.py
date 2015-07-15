e2c44ab1a9321e12fa8edba90ee0d09897dcc36a
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-15 17:53:39 -0400

    Add data for 80,000 users

--- scraper.py
+++ scraper.py
@@ -5,27 +5,30 @@ c = {}
 ls = {}
 cs = {}
 
-for line in open('users.txt'):
-    line = line.rstrip('\n')
-    profile = requests.get('https://github.com/' + line)
-    data = profile.text
-    soup = BeautifulSoup(data)
-    stats = soup.find_all("span", {"class" : "contrib-number"})
-    if stats:
-        contributions = int(stats[0].string.split()[0].replace(",", ""))
-        longest_streak = int(stats[1].string.split()[0].replace(",", ""))
-        current_streak = int(stats[2].string.split()[0].replace(",", ""))
-        c[line] = contributions
-        ls[line] = longest_streak
-        cs[line] = longest_streak
-    print "User:", line
-    if stats:
-        print "Total Contributions:", contributions
-        print "Longest Streak:", longest_streak
-        print "Current Streak:", current_streak
-        print
-    else:
-        print
+with open('users.txt') as f:
+    for i in xrange(3046):
+        f.next()
+    for line in f:
+        line = line.rstrip('\n')
+        profile = requests.get('https://github.com/' + line)
+        data = profile.text
+        soup = BeautifulSoup(data)
+        stats = soup.find_all("span", {"class" : "contrib-number"})
+        if stats:
+            contributions = int(stats[0].string.split()[0].replace(",", ""))
+            longest_streak = int(stats[1].string.split()[0].replace(",", ""))
+            current_streak = int(stats[2].string.split()[0].replace(",", ""))
+            c[line] = contributions
+            ls[line] = longest_streak
+            cs[line] = longest_streak
+        print "User:", line
+        if stats:
+            print "Total Contributions:", contributions
+            print "Longest Streak:", longest_streak
+            print "Current Streak:", current_streak
+            print
+        else:
+            print
 
 print "Top Contributions"
 count = 0
