f6cbc5d86fcd825abfbf1c958b6322f93bfa1dc2
Jackie Luo <jacquelineluo95@gmail.com>
2015-07-14 22:06:12 -0400

    Add 150,000 more users :whale:

--- api.py
+++ api.py
@@ -1,12 +1,18 @@
+import os
 import requests
 
-p = "3182"
+
+users = open("users.txt", "a")
+p = "161355"
 
 while True:
-    url = "https://api.github.com/users?since=" + p
+    url = ("https://api.github.com/users?client_id=" +
+            os.environ['CLIENT_ID'] + "&client_secret=" +
+            os.environ['CLIENT_SECRET'] + "&since=" + p)
     response = requests.get(url)
     response_dict = response.json()
     for r in response_dict:
+        users.write(r["login"] + "\n")
         print r["login"], r["id"]
         if r["id"] > int(p):
             p = str(r["id"])
